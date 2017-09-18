30971c141d4462ad5402ffe300b5de9e73886b0f
FlyStorm <ggloading@gmail.com>
2017-09-19 06:48:57 +0800

    导航页面，使用Handler的postDelayed方法，延时两秒进入到主页面

--- app/src/main/java/com/myshopdemo/activity/WelcomeActivity.java
+++ app/src/main/java/com/myshopdemo/activity/WelcomeActivity.java
@@ -1,13 +1,31 @@
 package com.myshopdemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class WelcomeActivity extends AppCompatActivity {
+/**
+ * 欢迎界面
+ * 功能：延时两秒进入到主页面
+ */
+public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //两秒进入主页面
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //在主线程中执行
+                //启动主页面
+                startActivity(new Intent(WelcomeActivity.this,MainActivity.class));
+                //关闭当前页面
+                finish();
+            }
+        },2000);
     }
 }
