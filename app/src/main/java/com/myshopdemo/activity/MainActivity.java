87ad3d59d607f1ae9a15ca869e70216d2ad7df81
FlyStorm <ggloading@gmail.com>
2017-09-19 08:54:48 +0800

    Butterknife框架的使用，替代findViewByid,提高性能

--- app/src/main/java/com/myshopdemo/activity/MainActivity.java
+++ app/src/main/java/com/myshopdemo/activity/MainActivity.java
@@ -2,12 +2,34 @@ package com.myshopdemo.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+    @Bind(R.id.fl)
+    FrameLayout mFl;
+    @Bind(R.id.rb_main)
+    RadioButton mRbMain;
+    @Bind(R.id.rb_type)
+    RadioButton mRbType;
+    @Bind(R.id.rb_find)
+    RadioButton mRbFind;
+    @Bind(R.id.rb_cart)
+    RadioButton mRbCart;
+    @Bind(R.id.rb_center)
+    RadioButton mRbCenter;
+    @Bind(R.id.rg)
+    RadioGroup  mRg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
