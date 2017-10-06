ca257c2139f9a7f104c45426c7cf37f6e4d1cfb6
FlyStorm <ggloading@gmail.com>
2017-10-06 10:24:05 +0800

    购物车页面布局分析实现

--- app/src/main/java/com/myshopdemo/shoppingcart/fragment/ShoppingcartFragment.java
+++ app/src/main/java/com/myshopdemo/shoppingcart/fragment/ShoppingcartFragment.java
@@ -1,11 +1,14 @@
 package com.myshopdemo.shoppingcart.fragment;
 
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
 
+import com.myshopdemo.R;
 import com.myshopdemo.base.BaseFragment;
+import com.myshopdemo.home.bean.GoodsBean;
+import com.myshopdemo.shoppingcart.utils.CartStorage;
+
+import java.util.List;
 
 /**
  * 创建者     yangyanfei
@@ -18,20 +21,27 @@ import com.myshopdemo.base.BaseFragment;
  * 更新描述   ${TODO}
  */
 public class ShoppingcartFragment extends BaseFragment {
-    TextView textView;
+//    TextView textView;
 
     @Override
     public View initView() {
         Log.e("TAG", "购物车页面的Fragment的UI被初始化了");
-        textView=new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        return textView;
+//        textView=new TextView(mContext);
+//        textView.setGravity(Gravity.CENTER);
+//        textView.setTextSize(25);
+
+        View view=View.inflate(mContext, R.layout.fragment_shopping_cart,null);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("购物车页面内容");
+//        textView.setText("购物车页面内容");
+
+        List<GoodsBean> goodsBeanList= CartStorage.getInstance().getAllData();
+        for (int i = 0; i < goodsBeanList.size(); i++) {
+            Log.e("TAG","ShoppingcartFragment:获取到的存储的商品数据=="+goodsBeanList.get(i).toString());
+        }
     }
 }
